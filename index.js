f6ae5e35117520d3b025bc5b2342a0382e888388
StevenIndigo <steven.gouasbault@hotmail.fr>
2016-06-20 20:42:12 +0800

    Ajout module.export

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var execFile = require('child_process').execFile,
     rebootW = execFile('cmd.exe', ['/c', 'my.bat']);
 //var rebootL = require('reboot').rebootImmediately();
 
+module.exports.reboot = function(){
+// do the work for windows or linux
+};
 console.log("Quel est mon système ?");
 
 // OS type
@@ -13,21 +16,24 @@ console.log('type : ' + os.type());
 // Possible values are 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'.
 console.log('platform : ' + os.platform());
 
-if (os.platform() == "win32") {
-  console.log("Tu es sous Windows");
-  rebootW.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
-  });
+reboot()
+{
+  if (os.platform() == "win32") {
+    console.log("Tu es sous Windows");
+      rebootW.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+      });
 
-  rebootW.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-  });
+      rebootW.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+      });
 
-  rebootW.on('exit', function (code) {
-    console.log('child process exited with code ' + code);
-  });}
+      rebootW.on('exit', function (code) {
+        console.log('child process exited with code ' + code);
+      });
 
-else if (os.platform() == "linux"){
+  else if (os.platform() == "linux"){
     console.log("Tu es sous Linux");
-//    rebootL();
+  //    rebootL();
+  }
 }
