f19c772cc23b303f2601ad2ab09c3fa203595ee3
StevenIndigo <steven.gouasbault@hotmail.fr>
2016-06-20 19:48:18 +0800

    Script-Reboot
    - Reboot Windows Fonctionnel
    - my. bat nécessaire pour Windows
    - Reboot Linux KO (Problème lib Reboot)

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-// Note: Delete me
-console.log('OS-Reboot: Hello world');
+var os = require("os");
+var execFile = require('child_process').execFile,
+    rebootW = execFile('cmd.exe', ['/c', 'my.bat']);
+//var rebootL = require('reboot').rebootImmediately();
+
+console.log("Quel est mon système ?");
+
+// OS type
+console.log('type : ' + os.type());
+
+// OS platform
+// Returns the operating system platform.
+// Possible values are 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'.
+console.log('platform : ' + os.platform());
+
+if (os.platform() == "win32") {
+  console.log("Tu es sous Windows");
+  rebootW.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+  });
+
+  rebootW.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+
+  rebootW.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
+  });}
+
+else if (os.platform() == "linux"){
+    console.log("Tu es sous Linux");
+//    rebootL();
+}
